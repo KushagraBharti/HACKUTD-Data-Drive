be96fa59099c957ed4f43b6e762fee10c8f964f7
Kushagra Bharti <kushagrabharti@gmail.com>
2024-11-17 07:08:11 -0600

    cleaned up

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import LandingPage from './LandingPage';
-import FuelEfficiencyPrediction from './components/FuelEfficiencyPrediction';
-import ClusterInsights from './components/ClusterInsights';
-import FuelEfficiencyChart from './components/FuelEfficiencyChart';
 
 // App component
 const App: React.FC = () => {
@@ -13,14 +10,6 @@ const App: React.FC = () => {
         <Routes>
           <Route path="/" element={<LandingPage />} />
         </Routes>
-        <div className="app min-h-screen bg-gray-100 flex items-center justify-center">
-          <FuelEfficiencyPrediction />
-        <div className="app min-h-screen bg-gray-100 flex items-center justify-center">
-          <ClusterInsights />
-          <h1>Fuel Efficiency Predictive Analysis</h1>
-          <FuelEfficiencyChart />
-      </div>
-      </div>
       </div>
     </Router>
   );
